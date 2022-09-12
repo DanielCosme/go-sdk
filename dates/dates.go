ee3312b30b2c2cd9d2b215f0660980b4b94c4a0d
Daniel Cosme <cosmedaniel8@gmail.com>
2022-09-12 09:09:13 -0400

    Add to beginning and end of the week

--- dates/dates.go
+++ dates/dates.go
@@ -10,14 +10,33 @@ func ToLocal(t time.Time, loc *time.Location) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), loc)
 }
 
-func ToPtr(t time.Time) *time.Time {
+func Ptr(t time.Time) *time.Time {
 	return &t
 }
 
-func ToBeginningOfDay(t time.Time) time.Time {
+func BeginningOfDay(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
 }
 
-func ToEndOfDay(t time.Time) time.Time {
+func EndOfDay(t time.Time) time.Time {
 	return time.Date(t.Year(), t.Month(), t.Day(), 23, 59, 0, 0, t.Location())
 }
+
+func StartAndEndOfweek(t time.Time) (time.Time, time.Time) {
+    return BeginningOfDay(t), EndOfWeek(t)
+}
+
+func BeginningOfWeek(t time.Time) time.Time {
+    for t.Weekday() != time.Monday {
+        t = t.AddDate(0, 0, -1)
+    }
+    return t
+}
+
+func EndOfWeek(t time.Time) time.Time {
+    for t.Weekday() != time.Sunday {
+        t = t.AddDate(0, 0, 1)
+    }
+    return t
+}
+
